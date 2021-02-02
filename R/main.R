f8a6578cfe5ecdf829f7946dd280582ba7d9ee5e
Sebastian Drost <s.drost@52north.org>
2021-02-02 12:19:52 +0100

    Add main script

---
+++ R/main.R
@@ -0,0 +1,19 @@
+source("./load_data.R")
+source("./anaysis.R")
+
+catchment_file <- "C:/Users/Sebastian/Documents/Projekte/Promotion/02_GIS/land_cover_wv/ezgs/pegel_ezgs.gpkg"
+raster_file <- "C:/Users/Sebastian/Documents/Projekte/Promotion/02_GIS/land_cover_wv/u2000_clc1990_v2020_20u1_wv.tif"
+
+# Read catchment polgons and land cover raster
+catchments <- load_catchments_as_sf(catchment_file)
+land_cover <- load_land_cover_rasters(raster_file)
+
+# Calculate the land cover class frequencies for each catchment
+res <- calculate_land_cover_frequency(catchments, land_cover, TRUE)
+
+# Check if the relative class frequency for each catchment sums up to 1 should
+# be TRUE.
+res %>%
+  group_by(id) %>%
+  summarise(sum = sum(freq)) %>% 
+  filter(sum != 1) %>% nrow() == 0

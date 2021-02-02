d16c1aa7d27e5add9befbcb3d9cfff2f8be22e68
Sebastian Drost <s.drost@52north.org>
2021-02-02 12:19:23 +0100

    Load function for raster loading

--- R/load_data.R
+++ R/load_data.R
@@ -1,12 +1,41 @@
+library(raster)
+library(sf)
+
 #' Loads catchment datasets from a spatial data source
-#' 
+#'
 #'  @param path Path to the spatial data source.
-#'  If data source is a shapefile, give the path to the directory containing the shapefile.
+#'  If data source is a shapefile, give the path to the directory containing
+#'  the shapefile.
 #'  If data source is a geopackage, give the path to the *.gpkg file.
-#'  
+#'
 #'  @param layer Name of the layer containing the catchment dataset
-#'  
-#'  @return catchment datasets
+#'
+#'  @return catchment datasets as simple feature object
 load_catchments <- function(path, layer) {
   rgdal::readOGR(dsn = path, layer = layer)
+}
+
+#' Loads catchment datasets from a spatial data source
+#'
+#'  @param path Path to the spatial data source.
+#'  If data source is a shapefile, give the path to the directory containing
+#'  the shapefile.
+#'  If data source is a geopackage, give the path to the *.gpkg file.
+#'
+#'  @param layer Name of the layer containing the catchment dataset
+#'
+#'  @return catchment datasets as simple feature object
+load_catchments_as_sf <- function(path, layer) {
+  sf::st_read(dsn = path,
+              layer = layer,
+              stringsAsFactors = FALSE)
+}
+
+#' Loads land cover classification data as a Raster object
+#'
+#' @param path Path to the Raster file
+#'
+#' @return Raster objects that represents the land cover classes
+load_land_cover_rasters <- function(path) {
+  raster::raster(path)
 }

b0d9a4e81c0258ddb57edc29fac0a789323b74a3
Sebastian Drost <s.drost@52north.org>
2021-02-11 19:16:15 +0100

    Use more clear variable names

--- R/main.R
+++ R/main.R
@@ -2,15 +2,18 @@ source("./R/data_io.R")
 source("./R/preprocess.R")
 source("./R/plot.R")
 
-catchment_file <- "C:/Users/Sebastian/Documents/Projekte/Promotion/02_GIS/land_cover_wv/ezgs/pegel_ezgs.gpkg"
-raster_file <- "C:/Users/Sebastian/Documents/Projekte/Promotion/02_GIS/land_cover_wv/u2000_clc1990_v2020_20u1_wv.tif"
+# Set path to required datasets
+basin_file <- "./data/wv_basin.geojson"
+subbasin_file <- "./data/wv_subbasins.geojson"
+raster_file <- "./data/land_cover.tif"
 
-# Read catchment polgons and land cover raster
-catchments <- load_catchments_as_sf(catchment_file)
+# Read catchment polygons and land cover raster
+basin <- load_catchments_as_sf(basin_file)
+subbasins <- load_catchments_as_sf(subbasin_file)
 land_cover <- load_land_cover_rasters(raster_file)
 
 # Calculate the land cover class frequencies for each catchment
-res <- calculate_land_cover_frequency(catchments, land_cover, TRUE)
+res <- calculate_land_cover_frequency(subbasins, land_cover, TRUE)
 
 # Check if the relative class frequency for each catchment sums up to 1 should
 # be TRUE.
@@ -18,3 +21,10 @@ res %>%
   group_by(id) %>%
   summarise(sum = sum(freq)) %>% 
   filter(sum != 1) %>% nrow() == 0
+
+# Select a catchment and plot its landcover class frequencies
+subbasin <- subbasins %>% filter(id == c(100060))
+landcover_classes <- res %>% filter(id == c(100060))
+
+plot_landcover_frequency(basin, subbasin, landcover_classes, "bar")
+

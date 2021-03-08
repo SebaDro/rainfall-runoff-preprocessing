c84ff4ceaae68d0216f02c9f680d207fc48bdc8b
Sebastian Drost <s.drost@52north.org>
2021-03-08 12:53:26 +0100

    Enhance REGNIE preprocessing script

--- R/run_regnie_preprocessing.R
+++ R/run_regnie_preprocessing.R
@@ -1,10 +1,21 @@
+# This script provides batch preprocessing of daily DWD REGNIE datasets for
+# multiple years. Adjust the parameters for your needs and run the remaining
+# code lines. 
+# Note, finetuning the performance of the script is still pending. Thus, you may
+# run into memory issues if processing is done for too many years at once. It may
+# help to split up the processing into smaller chunks by reducing the time frame.
+
 source("./R/setup.R")
 
-subbasin_file <- "./data/wv_subbasins.geojson"
-subbasins <- load_catchments_as_sf(subbasin_file) %>% st_transform(st_crs(4326))
+# Parameters
+output_path <- "./output/" # output path to save results
+subbasin_file <- "./data/wv_subbasins.geojson" 
+base_path <- "/path/to/regnie/" # must be absolute
+start_year <- 1990
+end_year <- 2020
 
-base_path <- "/path/to/regnie/" #Must be absolute
-paths <- paste0(base_path, "ra", 2010:2020, "m")
+subbasins <- load_catchments_as_sf(subbasin_file) %>% st_transform(st_crs(4326))
+paths <- paste0(base_path, "ra", 1990:2020, "m")
 
 for (path in paths) {
   cat(sprintf("\nRead files from folder %s\n", path))
@@ -13,6 +24,25 @@ for (path in paths) {
   res <- res %>% rename(precipitation = mean)
   
   # Note: precipitation has unit 1/10 mm
-  write.csv(res, file = paste0("./output/precipitation", basename(path), ".csv"), row.names = FALSE)
+  write.csv(res, file = paste0(output_path,"precipitation", basename(path), ".csv"), row.names = FALSE)
   cat(sprintf("\nReading files from folder %s done!\n", path))
 }
+
+# After saving the precipitation means for each single year, we have to merge the
+# single files all together and save as a single file
+paths <- paste0(base_path, "precipitation_ra", 1990:2020, "m.csv")
+res <- data.frame()
+for(path in paths) {
+  data <- read_csv(path, col_types = cols(catchment_id = col_character()))
+  res <- res %>%  bind_rows(data)
+}
+
+# Since precipitation values have unit 1/10 mm we convert to mm before storing and
+# round to two decimal places. In addition, we rearrange the columns to have a
+# separate column for each basin in order to have a more compact storage format
+res <- res %>%
+  mutate(precipitation = precipitation / 10) %>%
+  mutate(precipitation = round(precipitation, 2)) %>% 
+  pivot_wider(names_from = catchment_id, values_from = precipitation)
+
+write_csv(res, paste0(output_path, "precipitation_timeseries.csv"))

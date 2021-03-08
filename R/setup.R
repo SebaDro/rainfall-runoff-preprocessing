a64ff80fdbc6b244f2720152cf169e7ab1ea80b7
Sebastian Drost <s.drost@52north.org>
2021-03-08 13:36:14 +0100

    Rearrange setup

--- R/setup.R
+++ R/setup.R
@@ -1,10 +1,12 @@
-source("./R/data_io.R")
-source("./R/plot.R")
-source("./R/preprocess.R")
-
+# Install required packages
 if(!require(gridExtra)) install.packages('gridExtra')
 if(!require(raster)) install.packages('raster')
 if(!require(sf)) install.packages('sf')
 if(!require(stars)) install.packages('stars')
 if(!require(tidyverse)) install.packages('tidyverse')
 if(!require(tools)) install.packages('tools')
+
+# Load user scripts
+source("./R/data_io.R")
+source("./R/plot.R")
+source("./R/preprocess.R")

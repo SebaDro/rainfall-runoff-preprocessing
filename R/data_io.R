8f2c2c8c8bf70ed04ab7e1586b857b5d14d8c443
Sebastian Drost <s.drost@52north.org>
2021-03-08 13:43:21 +0100

    Remove unused function

--- R/data_io.R
+++ R/data_io.R
@@ -14,20 +14,6 @@ library(tools)
 #'  @param layer Name of the layer containing the catchment dataset
 #'
 #'  @return catchment datasets as simple feature object
-load_catchments <- function(path, layer) {
-  rgdal::readOGR(dsn = path, layer = layer)
-}
-
-#' Loads catchment datasets from a spatial data source
-#'
-#'  @param path Path to the spatial data source.
-#'  If data source is a shapefile, give the path to the directory containing
-#'  the shapefile.
-#'  If data source is a geopackage, give the path to the *.gpkg file.
-#'
-#'  @param layer Name of the layer containing the catchment dataset
-#'
-#'  @return catchment datasets as simple feature object
 load_catchments_as_sf <- function(path, layer) {
   sf::st_read(dsn = path,
               layer = layer,

4e10762c215898a68bcc443adcf80307cca2a6cd
Sebastian Drost <s.drost@52north.org>
2021-04-01 11:43:50 +0200

    Enhance setup script

--- R/setup.R
+++ R/setup.R
@@ -6,6 +6,7 @@ if(!require(stars)) install.packages('stars')
 if(!require(tidyverse)) install.packages('tidyverse')
 if(!require(tools)) install.packages('tools')
 if(!require(ncdf4)) install.packages('ncdf4')
+if(!require(ncdf4)) install.packages('units')
 
 # Load user scripts
 source("./R/data_io.R")
